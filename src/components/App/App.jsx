12ee58dc05ee7f6f3aaaf002e0190f58664aec4e
MAKCIMELIANO <makcimeliano@gmail.com>
2023-06-21 14:06:18 +0300

    profile component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,18 @@
+import { Profile } from '../Profile/Profile';
+import user from '../../data/user.json';
+
 export const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
 };
