4f2b7439dd0e47578847d9913ad26aaf19c07291
MAKCIMELIANO <makcimeliano@gmail.com>
2023-06-23 16:29:49 +0300

    TransactionHistory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,10 @@ import { Profile } from '../Profile/Profile';
 import user from '../../data/user.json';
 import stats from '../../data/data.json';
 import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 import { Statistics } from '../Statistics/Statistics';
 import { FriendList } from '../FriendList/FriendList';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -19,6 +21,7 @@ export const App = () => {
       />
       <Statistics title="upload stats" stats={stats} />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,38 @@
+import css from '../TransactionHistory/TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+export const TransactionHistory = ({ transactions }) => {
+  return (
+    <div>
+      <table className={css.transactionhistory}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.type.toUpperCase()}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
